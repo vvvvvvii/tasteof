a8f2abe8f0116e97af1d08249b2b8000242914b5
圈圈 <vvvvvvii@yahoo.com.tw>
2021-07-01 22:48:36 +0800

    調整前台產品列表頁：新增 tag 區、補行程長度 select 選項、讓產品顯示最低價及對應單位

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -5,24 +5,37 @@
         <div class="col-4">
           <div class="searchBox">
             <div class="searchBox-header">
-              <h3 class="h2">搜尋</h3>
+              <h3 class="fw-bold">搜尋</h3>
             </div>
             <div class="searchBox-body">
               <input type="text" placeholder="目的地" class="mb-3" />
-              <select name="activityPeriod" class="mb-5">
-                <option value="">行程長度</option>
+              <select name="activityPeriod" class="mb-6">
+                <option value="default" selected disabled>行程長度</option>
+                <option value="lessThan4hrs">小於 4 小時</option>
+                <option value="lessThan8hrs">4 - 8 小時</option>
+                <option value="2DaysTrip">兩天一夜</option>
+                <option value="3DaysTrip">三天兩夜</option>
               </select>
-              <div class="pb-5 mb-5 border-bottom border-gray">
-                <h4 class="mb-5">預算</h4>
+              <div class="pb-6 mb-6 border-bottom border-gray">
+                <h4 class="mb-6 h3">預算</h4>
                 <vue-slider
                   v-model="budget"
                   v-bind="options"
                   :tooltip-formatter="'NT {value}'"
                 ></vue-slider>
               </div>
-              <ul>
-                <li class="tag tag-outline-secondary">#城市導覽</li>
-              </ul>
+              <div
+                class="d-flex flex-wrap btn-group"
+                role="group"
+                aria-label="Basic checkbox toggle button group"
+              >
+                <div class="me-2 mb-2" v-for="(item, key) in tagCategory" :key="key">
+                  <input type="checkbox" :id="item" autocomplete="off" class="btn-check" />
+                  <label :for="item" class="btn btn-outline-secondary rounded-3"
+                    ># {{ item }}
+                  </label>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -47,8 +60,8 @@
                     <p>{{ item.description }}</p>
                   </div>
                   <p class="text-end">
-                    <span class="h3">NT {{ item.price }} 起</span>
-                    / {{ item.unit }}
+                    <span class="h3">NT {{ item.lowestPrice }} 起</span>
+                    / {{ item.lowestPriceUnit }}
                   </p>
                 </div>
               </router-link>
@@ -86,42 +99,29 @@ export default {
         tooltip: 'active',
         tooltipPlacement: 'top',
       },
+      tagCategory: [
+        '雙北',
+        '中彰投',
+        '嘉南',
+        '高屏',
+        '花東',
+        '城市導覽',
+        '票券體驗',
+        '包車服務',
+        '親子',
+        '浪漫',
+        '冒險',
+        '自然',
+        '烹飪',
+        '美食',
+        '知性文化',
+        '水上活動',
+        '獨家代理',
+      ],
       pagination: {},
     };
   },
   components: { alert, pagination, VueSlider },
-  created() {
-    this.$http
-      .get(
-        `${process.env.VUE_APP_API}/api/${
-          process.env.VUE_APP_PATH
-        }/products/?page=${(this.pagination.current_page = 1)}`,
-      )
-      .then((res) => {
-        if (res.data.success) {
-          const { data } = res;
-          this.products = data.products;
-          this.pagination = data.pagination;
-        } else {
-          this.customAlert(res.data.message);
-        }
-      })
-      .catch((err) => {
-        this.customAlert(err.response);
-      });
-    this.$http
-      .get(`${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/products/all`)
-      .then((res) => {
-        if (res.data.success) {
-          this.totalProducts = res.data.products;
-        } else {
-          this.customAlert(res.data.message);
-        }
-      })
-      .catch((err) => {
-        this.customAlert(err.response);
-      });
-  },
   methods: {
     getData(currentPage = 1) {
       // 若未傳入則預設為第一頁
@@ -132,7 +132,34 @@ export default {
         .then((res) => {
           if (res.data.success) {
             this.products = res.data.products;
+            this.products.forEach((item, index) => {
+              // 個別取出每個產品
+              let packageOptionsPrice = item.packageOptions.map((i) => i.price); // 取出該產品的所有方案中的價格
+              packageOptionsPrice = packageOptionsPrice.sort((x, y) => x - y); // 價格小排到大
+              // this.products[index].lowestPrice = packageOptionsPrice[0];
+              const [lowestPrice] = packageOptionsPrice; // 該產品最低價格為陣列第一個數
+              this.products[index].lowestPrice = lowestPrice;
+              // 找到該價格對應的單位
+              item.packageOptions.forEach((i) => {
+                if (i.price === lowestPrice) {
+                  this.products[index].lowestPriceUnit = i.unit;
+                }
+              });
+            });
+            console.log(this.products);
             this.pagination = res.data.pagination;
+          } else {
+            this.customAlert(res.data.message);
+          }
+        })
+        .catch((err) => {
+          this.customAlert(err.response);
+        });
+      this.$http
+        .get(`${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/products/all`)
+        .then((res) => {
+          if (res.data.success) {
+            this.totalProducts = res.data.products;
           } else {
             this.customAlert(res.data.message);
           }
@@ -148,6 +175,9 @@ export default {
     closeCustomAlert() {
       this.showAlert = false;
     },
+  },
+  created() {
+    this.getData();
   },
 };
 </script>
